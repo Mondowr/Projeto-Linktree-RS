51015710575a57f1c8336e562740e0dc9c27066e
Patrick Artigas <patrickartigas@MacBook-Air-de-Patrick.local>
2024-10-09 21:20:24 -0300

    :Feito: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+  
+  //substituir a imagem
+  if(html.classList.contains("light")) {
+    //se tiver light mode, adc a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src', './assets/avatar.png')
+  }
+  
+  //if(html.classList.contains('light')) {
+    //html.classList.remove('light')
+  //} else {
+    //html.classList.add('light')}
+    //Tudo isso aqui pode ser feito somente com essa função "toggle" do js
+}
